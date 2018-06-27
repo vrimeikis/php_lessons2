c693da4be72890675cd6ae801e8468f64e9a50e0
vytautas <vytautas.rimeikis@gmail.com>
2018-06-27 13:02:39 +0300

    june 26 lesson use composer

---
+++ jun26/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types = 1);
+
+define('ROOT_PATH', __DIR__);
+
+require_once ROOT_PATH . '/vendor/autoload.php';
---
+++ jun26/index.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types = 1);
+
+use Hello\SayHello;
+
+require_once 'bootstrap.php';
+
+echo SayHello::world();
